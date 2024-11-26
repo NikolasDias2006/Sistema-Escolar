3d832a97665798f6478f75d01cd778526e436718
Nikolas_Dev <131534037+NikolasDias2006@users.noreply.github.com>
2024-11-26 20:53:32 -0300

    database.py

--- app/database.py
+++ app/database.py
@@ -1,1 +1,9 @@
-from sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://admin:password@localhost:5432/school"
+
+engine = create_engine(DATABASE_URL)
+Sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
